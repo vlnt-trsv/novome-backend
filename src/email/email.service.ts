02e410c870511c6bd7c1708573b6ede69fcd598a
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2026-01-28 15:02:51 +0900

    feat(email): Добавлен метод для отправки письма (Доступ)

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@nestjs/common"
-import nodemailer from "nodemailer"
+import nodemailer, { SentMessageInfo } from "nodemailer"
 
 @Injectable()
 export class EmailService {
-  async sendConfirmationEmail(email: string, token: string) {
-    const transporter = nodemailer.createTransport({
+  private readonly _transporter: nodemailer.Transporter<SentMessageInfo>
+
+  constructor() {
+    this._transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: Number(process.env.EMAIL_PORT),
       secure: process.env.EMAIL_SECURE === "true",
@@ -13,16 +15,26 @@ export class EmailService {
         pass: process.env.EMAIL_PASSWORD,
       },
     })
-    // TODO: (SendGrid, Mailgun, AWS SES, или свой SMTP)
-    // const smtpServerEmail = await () => {}
+  }
+  async sendConfirmationEmail(email: string, token: string): Promise<SentMessageInfo> {
     const url = `${process.env.API_URL}/auth/confirm?email=${email}&token=${token}`
 
-    const nodemailerEmail = await transporter.sendMail({
+    return await this._transporter.sendMail({
       from: `"Модерация Novome" ${process.env.EMAIL_USER}`,
       to: email,
       subject: "Подтверждение почты",
       html: `<b>Ваша ссылка для подтверждения почты \n${url}</b>`,
     })
-    return nodemailerEmail
+  }
+
+  async sendRecoveryEmail(email: string, token: string): Promise<SentMessageInfo> {
+    const url = `${process.env.API_URL}/auth/reset-password?email=${email}&token=${token}`
+
+    return await this._transporter.sendMail({
+      from: `"Модерация Novome" ${process.env.EMAIL_USER}`,
+      to: email,
+      subject: "Восстановление доступа",
+      html: `<b>Ваша ссылка для восстановления доступа \n${url}</b>`,
+    })
   }
 }
