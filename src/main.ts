4185bd6fc49e209ba77974e5c0c7add588dd4bb7
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2026-01-28 12:42:14 +0900

    refactor(main): добавлен cors, cookieParser

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,25 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 import { ValidationPipe } from "@nestjs/common"
+import cookieParser from "cookie-parser"
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface"
 
 const PORT = process.env.PORT ?? 5000
 const PREFIX = "api/v1"
+
+const cors: CorsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+}
 
 async function start() {
   const app = await NestFactory.create(AppModule)
 
   app.setGlobalPrefix(PREFIX)
   app.useGlobalPipes(new ValidationPipe())
+  app.enableCors(cors)
+  app.use(cookieParser())
 
   const config = new DocumentBuilder()
     .setTitle("REST API V1")
