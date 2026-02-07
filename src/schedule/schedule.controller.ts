a5fbf4a79d1c4512989bc6964db55c4d49d98e18
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2026-02-07 22:44:24 +0900

    refactor(schedule)

--- src/schedule/schedule.controller.ts
+++ src/schedule/schedule.controller.ts
@@ -5,25 +5,12 @@ import { User } from "@prisma/client"
 import { JwtAuthGuard } from "src/auth/guards/jwt.guard"
 
 @Controller("schedules")
-@UseGuards(JwtAuthGuard)
+// @UseGuards(JwtAuthGuard)
 export class ScheduleController {
   constructor(private scheduleService: ScheduleService) {}
 
-  // @Get(":id")
-  // async getSchedules(
-  //   @Param("id") targetId: string,
-  //   @Query("from") from: string,
-  //   @Query("to") to: string,
-  // ) {
-  //   return await this.scheduleService.getSchedules(targetId, from, to)
-  // }
-
-  @Post(":id")
-  async generateSlots(
-    @Param("id") targetId: string,
-    @Query("date") date: Date,
-    @Query("constraintId") constraintId: string,
-  ) {
-    return await this.scheduleService.generateSlots(targetId, date, constraintId)
+  @Get(":id")
+  async getSchedules(@Param("id") targetId: string, @Query("date") date: Date) {
+    return await this.scheduleService.getSchedules(targetId, date)
   }
 }
