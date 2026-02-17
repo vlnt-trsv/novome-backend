ba7a5a5cabb1e55331fe3f3bef41947fce117af9
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2026-02-17 15:32:06 +0900

    feat(notification/listener): Добавлен слушатель для сообщений о связях

---
+++ src/notification/listeners/relationship-notification.listener.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common"
+import { OnEvent } from "@nestjs/event-emitter"
+import { Relationship } from "@prisma/client"
+import { RELATIONSHIP_NOTIFICATION_CONST } from "src/common/constants/relationship.constants"
+import { PrismaService } from "src/prisma/prisma.service"
+
+@Injectable()
+export class RelationshipNotificationListener {
+  constructor(private prisma: PrismaService) {}
+
+  @OnEvent("relationship.status.changed", { async: true })
+  async relationshipStatusChanged(payload: { userId: string; relationship: Relationship }) {
+    const { userId, relationship } = payload
+    const meta = RELATIONSHIP_NOTIFICATION_CONST[relationship.status]
+
+    return await this.prisma.notification.create({
+      data: {
+        userId,
+        type: meta.type,
+        title: meta.title,
+        description: meta.description,
+        payload: relationship,
+      },
+    })
+  }
+}
