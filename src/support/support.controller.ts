8f16ecc1ff4d684aa6879b6a59f1868b4cb3bd49
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2026-02-20 13:49:40 +0900

    feat(support): Добавлен сервис поддержки

---
+++ src/support/support.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common"
+import { SupportService } from "./support.service"
+import { CurrentUser } from "src/common/decorators/current-user.decorator"
+import { Ticket, TicketMessage, User } from "@prisma/client"
+import { JwtAuthGuard } from "src/auth/guards/jwt.guard"
+import { CreateTicketDto } from "./dto/create-ticket.dto"
+import { ReplyTicketDto } from "./dto/reply-ticket.dto"
+
+@Controller("support")
+@UseGuards(JwtAuthGuard)
+export class SupportController {
+  constructor(private supportService: SupportService) {}
+
+  @Get("tickets")
+  async getTickets(@CurrentUser() user: User): Promise<Ticket[]> {
+    return await this.supportService.getTickets(user.id)
+  }
+
+  @Get("tickets/:id")
+  async getTicket(@CurrentUser() user: User, @Param("id") id: string): Promise<Ticket> {
+    return await this.supportService.getTicket(user.id, id)
+  }
+
+  @Post("tickets")
+  async createTicket(
+    @CurrentUser() user: User,
+    @Body() createTicketDto: CreateTicketDto,
+  ): Promise<Ticket> {
+    return await this.supportService.createTicket(user.id, createTicketDto)
+  }
+
+  @Post("tickets/:id/reply")
+  async replyTicket(
+    @CurrentUser() user: User,
+    @Param("id") id: string,
+    @Body() replyTicketDto: ReplyTicketDto,
+  ): Promise<TicketMessage> {
+    return await this.supportService.replyTicket(user.id, id, replyTicketDto)
+  }
+
+  @Post("tickets/:id/close")
+  async closeTicket(@CurrentUser() user: User, @Param("id") id: string) {
+    return await this.supportService.closeTicket(user.id, id)
+  }
+}
---
+++ src/support/support.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from "@nestjs/common"
+import { Ticket, TicketMessage } from "@prisma/client"
+import { PrismaService } from "src/prisma/prisma.service"
+import { CreateTicketDto } from "./dto/create-ticket.dto"
+import { ReplyTicketDto } from "./dto/reply-ticket.dto"
+
+@Injectable()
+export class SupportService {
+  constructor(private prisma: PrismaService) {}
+
+  async getTickets(userId: string): Promise<Ticket[]> {
+    return await this.prisma.ticket.findMany({ where: { userId } })
+  }
+
+  async getTicket(userId: string, id: string): Promise<Ticket> {
+    return await this.prisma.ticket.findUniqueOrThrow({
+      where: { userId, id },
+      include: { messages: true },
+    })
+  }
+
+  async createTicket(userId: string, createTicketDto: CreateTicketDto): Promise<Ticket> {
+    const { title, text } = createTicketDto
+    return await this.prisma.ticket.create({
+      data: {
+        userId,
+        title,
+        messages: {
+          create: {
+            text,
+            senderId: userId,
+          },
+        },
+      },
+      include: {
+        messages: true,
+      },
+    })
+  }
+
+  async replyTicket(
+    userId: string,
+    id: string,
+    replyTicketDto: ReplyTicketDto,
+  ): Promise<TicketMessage> {
+    const { text } = replyTicketDto
+    return await this.prisma.$transaction(async (tx) => {
+      const ticket = await tx.ticket.findUnique({ where: { id } })
+      return await tx.ticketMessage.create({
+        data: {
+          senderId: userId,
+          text,
+          ticket: { connect: { id: ticket?.id } },
+        },
+      })
+    })
+  }
+
+  async closeTicket(userId: string, id: string) {
+    return await this.prisma.ticket.update({ where: { userId, id }, data: { status: "CLOSED" } })
+  }
+}
