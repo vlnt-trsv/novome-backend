a1160ab4f9768733aba91e421847bab48f235a69
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2026-02-20 13:49:10 +0900

    refactor(moderation): Добавлен сервис модерации

---
+++ src/moderation/moderation.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common"
+import { ModerationService } from "./moderation.service"
+import { ModerationController } from "./moderation.controller"
+
+@Module({
+  providers: [ModerationService],
+  controllers: [ModerationController],
+})
+export class ModerationModule {}
