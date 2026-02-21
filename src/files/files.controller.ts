26704a93a3a333d58e098f3a8dabfa23e11f95a8
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2026-02-21 20:20:30 +0900

    refactor(files)

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,32 +1,52 @@
-import { Controller, Post, UploadedFile, UseGuards, UseInterceptors } from "@nestjs/common"
-import { FileInterceptor } from "@nestjs/platform-express"
+import {
+  Body,
+  Controller,
+  Delete,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from "@nestjs/common"
+import { FileInterceptor, FilesInterceptor } from "@nestjs/platform-express"
 import { FilesService } from "./files.service"
 import { CurrentUser } from "src/common/decorators/current-user.decorator"
 import { JwtAuthGuard } from "src/auth/guards/jwt.guard"
 import { User } from "@prisma/client"
 import { AvatarValidationPipe } from "./pipe/avatar-validation.pipe"
 import { ConsentsRequiredGuard } from "src/consent/guards/consents-required.guard"
+import { FilesValidationPipe } from "./pipe/files-validation.pipe"
 
 @Controller("files")
 @UseGuards(JwtAuthGuard, ConsentsRequiredGuard)
 export class FilesController {
   constructor(private filesServies: FilesService) {}
 
-  //   @Post("upload-documents")
-  //   @UseInterceptors(FilesInterceptor("files", 10))
-  //   async uploadDocument(
-  //     @UploadedFiles(FileValidationPipe) files: Express.Multer.File[],
-  //     @CurrentUser() user: User,
-  //   ) {
-  //     return await this.filesServies.processFiles(files, user)
-  //   }
+  @Post("download")
+  async downloadFile(@Body("key") key: string) {
+    return await this.filesServies.downloadFile(key)
+  }
+
+  @Post("upload")
+  @UseInterceptors(FilesInterceptor("files"))
+  async uploadFiles(
+    @CurrentUser() user: User,
+    @UploadedFiles(FilesValidationPipe) files: Express.Multer.File[],
+  ) {
+    return await this.filesServies.uploadFiles(files, user)
+  }
 
   @Post("avatar")
   @UseInterceptors(FileInterceptor("avatar"))
   async uploadAvatar(
-    @UploadedFile(AvatarValidationPipe) avatar: Express.Multer.File,
     @CurrentUser() user: User,
+    @UploadedFile(AvatarValidationPipe) avatar: Express.Multer.File,
   ) {
     return await this.filesServies.uploadAvatar(avatar, user)
+  }
+
+  @Delete()
+  async deleteFile(@Body("key") key: string) {
+    return await this.filesServies.deleteFile(key)
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, StreamableFile } from "@nestjs/common"
 import { S3Service } from "../s3/s3.service"
 import { PrismaService } from "src/prisma/prisma.service"
-import { User } from "@prisma/client"
+import { File, Prisma, User } from "@prisma/client"
+import { Readable } from "node:stream"
+import { ROLE_CONST } from "src/common/constants/user.constants"
 
 @Injectable()
 export class FilesService {
@@ -9,6 +11,14 @@ export class FilesService {
     private s3: S3Service,
     private prisma: PrismaService,
   ) {}
+
+  async downloadFile(key: string) {
+    const response = await this.s3.download(key)
+
+    return new StreamableFile(response.Body as Readable, {
+      type: response.ContentType,
+    })
+  }
 
   async uploadAvatar(file: Express.Multer.File, user: User) {
     const fileKey = `${user.id}/avatar/${user.id}-avatar`
@@ -26,28 +36,45 @@ export class FilesService {
     }
   }
 
-  async processFiles(files: Express.Multer.File[], user: User) {
-    return await Promise.all(
-      files.map(async (file) => {
-        const date = new Date()
-        const year = date.getFullYear()
-        const month = date.getMonth()
-        const day = date.getDay()
-        const fileKey = `${user.id}/documents/${year}-${month}-${day}/${file.originalname}-${crypto.randomUUID()}`
-        const s3Data = await this.s3.upload(file, fileKey)
+  async uploadFiles(files: Express.Multer.File[], user: User, tx?: Prisma.TransactionClient) {
+    const prisma = tx ?? this.prisma
+    const uploadedData: File[] = []
+    const roleKey = ROLE_CONST[user.role]
 
-        return {
-          key: s3Data.key,
-          url: s3Data.url,
-          originalName: file.originalname,
-          mimeType: file.mimetype,
-          size: file.size,
-        }
-      }),
-    )
+    for (const file of files) {
+      const key = this._generateKey(user.id, "files", file.originalname)
+      const s3Data = await this.s3.upload(file, key)
+
+      try {
+        const dbFile = await prisma.file.create({
+          data: {
+            originalName: file.originalname,
+            mimeType: file.mimetype,
+            s3Key: s3Data.key,
+            size: file.size,
+            url: s3Data.url,
+            [`${roleKey}Id`]: user.id,
+          },
+        })
+        uploadedData.push(dbFile)
+      } catch (error) {
+        await this.s3.delete(s3Data.key)
+        throw error
+      }
+    }
+    return uploadedData
   }
 
-  async deleteMany(keys: string[]) {
-    return await this.s3.deleteMany(keys)
+  async deleteFile(key: string) {
+    return await this.prisma.$transaction(async (tx) => {
+      await tx.file.delete({ where: { s3Key: key } })
+      return await this.s3.delete(key)
+    })
+  }
+
+  private _generateKey(userId: string, folder: string, fileName: string): string {
+    const date = new Date()
+    const timestamp = `${date.getUTCFullYear()}-${date.getUTCMonth() + 1}-${date.getUTCDate()}`
+    return `${userId}/${folder}/${timestamp}/${crypto.randomUUID()}-${fileName}`
   }
 }
