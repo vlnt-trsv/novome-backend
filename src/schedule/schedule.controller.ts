47b9d87c0e61a83415ac417276dbecd058af00da
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2026-03-02 22:58:56 +0900

    refactor(schedule)

--- src/schedule/schedule.controller.ts
+++ src/schedule/schedule.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Get, Param, Query, UseGuards } from "@nestjs/common"
+import { Body, Controller, Get, Param, Post, Query, UseGuards } from "@nestjs/common"
 import { ScheduleService } from "./schedule.service"
 import { JwtAuthGuard } from "src/auth/guards/jwt.guard"
+import { CurrentUser } from "src/common/decorators/current-user.decorator"
+import { Break, ROLE, User, WorkRule } from "@prisma/client"
+import { CreateWorkRuleDto } from "./dto/create-work-rule.dto"
+import { Role } from "src/user/decorator/role.decorator"
+import { RoleGuard } from "src/user/guard/role.guard"
+import { CreateBreakDto } from "./dto/create-break.dto"
 
 @Controller("schedules")
 @UseGuards(JwtAuthGuard)
@@ -10,5 +16,36 @@ export class ScheduleController {
   @Get(":id")
   async getSchedules(@Param("id") targetId: string, @Query("date") date: Date) {
     return await this.scheduleService.getSchedules(targetId, date)
+  }
+
+  @Post("work-rule")
+  @Role(ROLE.DOCTOR)
+  @UseGuards(RoleGuard)
+  async createWorkRule(
+    @CurrentUser() user: User,
+    @Body() createWorkRuleDto: CreateWorkRuleDto,
+  ): Promise<WorkRule> {
+    return await this.scheduleService.createWorkRule(user.id, createWorkRuleDto)
+  }
+
+  @Post("break")
+  @Role(ROLE.DOCTOR)
+  @UseGuards(RoleGuard)
+  async createBreak(
+    @CurrentUser() user: User,
+    @Body() createBreakDto: CreateBreakDto,
+  ): Promise<Break> {
+    return await this.scheduleService.createBreak(user.id, createBreakDto)
+  }
+
+  @Post(":id")
+  @Role(ROLE.DOCTOR)
+  @UseGuards(RoleGuard)
+  async updateSchedule(
+    @CurrentUser() user: User,
+    @Param("id") id: string,
+    @Param("isAvailable") isAvailable: boolean,
+  ) {
+    return await this.scheduleService.updateSchedule(user.id, id, isAvailable)
   }
 }
